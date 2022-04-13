c8a23395b3edb1711aff51ab0b9392eb83eff56b
haseeb1540 <haseebkappanvga@gmail.com>
2022-04-13 23:54:06 +0530

    loader issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,9 +54,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "f90898f14537e8a333c5a8a82502ee1b",
-"/": "f90898f14537e8a333c5a8a82502ee1b",
-"main.dart.js": "37a9c3e3b847e597afad1899f53bc08c",
+"index.html": "ec64c4497b913bd866b24c70e1f87376",
+"/": "ec64c4497b913bd866b24c70e1f87376",
+"main.dart.js": "76297876435b0ef635a19725d57d30a3",
 "manifest.json": "4b9de85c116de6b72017882b7bd4c499",
 "version.json": "f9105f7af95f8c314f09a299deed7cd1"
 };
