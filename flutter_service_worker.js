b01339bc8969521c4a509bfa5f33874c17b03646
haseeb1540 <haseebkappanvga@gmail.com>
2022-04-14 01:51:16 +0530

    loader issue and container alignment

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,9 +54,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c62df46c19aa26170c9ee7858f1adc9f",
-"/": "c62df46c19aa26170c9ee7858f1adc9f",
-"main.dart.js": "f49b1cedf62101b261e369e78e068a4b",
+"index.html": "d5efb95944a3369700e2b9f06413ccf3",
+"/": "d5efb95944a3369700e2b9f06413ccf3",
+"main.dart.js": "c2a578ecec567278351d67ba07d293e9",
 "manifest.json": "4b9de85c116de6b72017882b7bd4c499",
 "version.json": "f9105f7af95f8c314f09a299deed7cd1"
 };
