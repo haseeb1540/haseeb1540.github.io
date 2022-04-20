d8c8c194273d69796da2d6b0f4f9802d882951c9
haseeb1540 <haseebkappanvga@gmail.com>
2022-04-21 00:39:09 +0530

    responsive design added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,9 +54,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "f8589e7bb63a50eac16f4d2000f640f9",
-"/": "f8589e7bb63a50eac16f4d2000f640f9",
-"main.dart.js": "e8b46bd18ec301206acd386663fe6003",
+"index.html": "cb46c2ea67874a56179e73f1c27e688e",
+"/": "cb46c2ea67874a56179e73f1c27e688e",
+"main.dart.js": "d8a455bc80a7f7571b44b051a24ccddd",
 "manifest.json": "4b9de85c116de6b72017882b7bd4c499",
 "version.json": "f9105f7af95f8c314f09a299deed7cd1"
 };
