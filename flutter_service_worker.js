c6cb47b5a1905d855c560ee5861ad9ce53dd55d9
haseeb1540 <haseebkappanvga@gmail.com>
2022-04-23 02:04:18 +0530

    responsive pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,9 +54,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "b30cdec62a684a0a77d136ae153279cc",
-"/": "b30cdec62a684a0a77d136ae153279cc",
-"main.dart.js": "fc09f0384efe9705a787fdf083345d37",
+"index.html": "6c308ca44fe698c090573c9b27084e8b",
+"/": "6c308ca44fe698c090573c9b27084e8b",
+"main.dart.js": "98f2bc6bcdecb3d96e99324384342fe6",
 "manifest.json": "4b9de85c116de6b72017882b7bd4c499",
 "version.json": "f9105f7af95f8c314f09a299deed7cd1"
 };
