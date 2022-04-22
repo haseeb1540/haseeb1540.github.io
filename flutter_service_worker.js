98b2c3f967f481c2eef7f0126ea1bdbc874898e4
haseeb1540 <haseebkappanvga@gmail.com>
2022-04-23 00:49:16 +0530

    responsive issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,9 +54,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7fd4beb700394f76a45c17594e3c7e4d",
-"/": "7fd4beb700394f76a45c17594e3c7e4d",
-"main.dart.js": "269aa4cf1d7e8df06e34e49dd95e91c8",
+"index.html": "af82b71b6603d363eba2d136f770c074",
+"/": "af82b71b6603d363eba2d136f770c074",
+"main.dart.js": "ae4d6f67207c0ccdc06a28d33c5b29b0",
 "manifest.json": "4b9de85c116de6b72017882b7bd4c499",
 "version.json": "f9105f7af95f8c314f09a299deed7cd1"
 };
